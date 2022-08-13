835661887e49912ff8b0892c33dcf81fe04f068b
tommy <tommyprn8@gmail.com>
2022-08-13 13:54:12 +0700

    add template pages

---
+++ frontend/src/pages/dashboard/index.js
@@ -0,0 +1,7 @@
+export const Dashboard = () => {
+  return (
+    <div className="Container">
+      <p>ini halaman Dashboard</p>
+    </div>
+  );
+};
---
+++ frontend/src/pages/login/index.js
@@ -0,0 +1,7 @@
+export const Login = () => {
+  return (
+    <div className="Container">
+      <p>ini halaman Login</p>
+    </div>
+  );
+};
